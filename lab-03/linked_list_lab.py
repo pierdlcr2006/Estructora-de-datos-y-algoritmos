ceabb195e1ab1745eb6428d331b62ebc23f6f305
rafaelchuco <rafaelchuco@github.com>
2025-04-02 08:43:32 -0500

    lab03inicial

---
+++ lab-03/linked_list_lab.py
@@ -0,0 +1,74 @@
+class Node:
+    def __init__(self):
+        pass
+    def get_data(self):
+        pass
+    def set_data(self, data):
+        pass
+    def get_next(self):
+        pass
+    def set_next(self, next_node):
+        pass
+
+class LinkedList:
+    def __init__(self):
+        pass
+    def display(self):
+        pass
+    def list_length(self):
+        pass
+    def insert_at_beginning(self, data):
+        pass
+    def insert_at_end(self, data):
+        pass
+    def insert_at_position(self, data, position):
+        pass
+    def delete_from_beginning(self):
+        pass
+    def delete_from_end(self):
+        pass
+    def delete_from_position(self, position):
+        pass
+    def search(self, key):
+        pass
+    def get_nth_from_end(self, n):
+        pass
+    def clear(self):
+        pass
+    def has_cycle(self):
+        pass
+    def reverse(self):
+        pass
+    def find_middle(self):
+        pass
+    def remove_duplicates(self):
+        pass
+
+def merge_sorted_lists(list1, list2):
+    pass
+
+class Queue:
+    def __init__(self):
+        pass
+    def is_empty(self):
+        pass
+    def enqueue(self, data):
+        pass
+    def dequeue(self):
+        pass
+    def peek(self):
+        pass
+    def size(self):
+        pass
+    def display(self):
+        pass
+
+def test_linked_list():
+    pass
+
+def test_queue():
+    pass
+
+def test_merge_sorted_lists():
+    pass
+
